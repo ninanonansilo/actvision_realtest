7316f20810729a1dc1772f00349cf50af270de1e
ninanonansilo <76138717+ninanonansilo@users.noreply.github.com>
2021-09-18 04:34:38 +0900

    Update schedule.py
    주석달아봣슴

--- movie/schedule.py
+++ movie/schedule.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 def diff_date(start, end):
     start = datetime.strptime(start, "%Y%m%d")
     end = datetime.strptime(end, "%Y%m%d")
+    #hihii
     dates = [(start + timedelta(days=i)).strftime("%Y%m%d") for i in range((end-start).days+1)]
 
 
